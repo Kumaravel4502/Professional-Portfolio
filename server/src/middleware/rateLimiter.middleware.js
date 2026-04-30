545bd1bff5227fb6d65606a12ff0d6e2d7d2590c
Kumaravel <kumaravel4502q@gmail.com>
2026-04-30 22:04:10 +0530

    Modified rateLimiter

--- server/src/middleware/rateLimiter.middleware.js
+++ server/src/middleware/rateLimiter.middleware.js
@@ -1,9 +1,20 @@
+// const rateLimit = require('express-rate-limit');
+
+// const contactRateLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5,
+//   message: { message: 'Too many requests from this IP, please try again later.' }
+// });
+
+// module.exports = { contactRateLimiter };
+
+
 const rateLimit = require('express-rate-limit');
 
 const contactRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
-  message: { message: 'Too many requests from this IP, please try again later.' }
+  max: 50,
+  message: "Too many requests, please try later.",
 });
 
-module.exports = { contactRateLimiter };
+module.exports = {contactRateLimiter};
