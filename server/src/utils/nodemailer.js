ed2d9831b86c2dfb27db62dcee927eb4c5a83ab1
Kumaravel <kumaravel4502q@gmail.com>
2026-05-02 11:15:40 +0530

    ..

--- server/src/utils/nodemailer.js
+++ server/src/utils/nodemailer.js
@@ -49,6 +49,8 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },  tls: {
+    rejectUnauthorized: false, // ✅ Fix for self-signed cert
   },
   family: 4,
 });
